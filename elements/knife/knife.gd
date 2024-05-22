5cb80b60bed15c35022807d1b28b48ce1f2b5bd0
dipresiya <stulchiksolnceluchik@gmail.com>
2024-05-22 12:26:17 +0300

    Initial commit

---
+++ elements/knife/knife.gd
@@ -0,0 +1,48 @@
+extends CharacterBody2D
+
+enum State { IDLE, FLY_TO_TARGET, FLY_AWAY}
+
+var state := State.IDLE
+
+var speed := 4500.0
+
+var fly_away_direction := Vector2.DOWN
+var fly_away_speed := 1000.0
+var fly_away_rotation_speed := 1500.0
+var fly_away_deviation := PI / 4.0
+
+func _physics_process(delta: float):
+	match state:
+		State.FLY_TO_TARGET:
+			var collision = move_and_collide(Vector2.UP * speed * delta)
+			if collision:
+				handle_collision(collision)
+		State.FLY_AWAY:
+			global_position += fly_away_direction * fly_away_speed * delta
+			rotation += fly_away_rotation_speed * delta
+	
+func change_state(new_state: State):
+	state = new_state
+	
+func  throw():
+	change_state(State.FLY_TO_TARGET)
+
+func throw_away(direction: Vector2):
+	var direction_deviaton = Globals.rng.randf_range(-fly_away_deviation, fly_away_deviation)
+	fly_away_direction = direction.rotated(direction_deviaton)
+	change_state(State.FLY_AWAY)
+	
+func handle_collision(collision: KinematicCollision2D):
+	var collider := collision.get_collider()
+	if collider is Target:
+		add_knife_to_target(collider)
+		change_state(State.IDLE)
+	else:
+		throw_away(collision.get_normal())
+
+func add_knife_to_target(target: Target):
+	get_parent().remove_child(self)
+	global_position = Target.KNIFE_POSITION
+	rotation = 0
+	target.add_object_with_pivot(self, -target.rotation)
+
